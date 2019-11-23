480ba2f733fc6bc4526d46db40bfddb2c5011f87
Leo <790868516@qq.com>
2019-11-24 00:50:13 +0800

    上传build

---
+++ build/precache-manifest.f39edfcddc292f561f26fada006142f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefecec3aa580df0a826d38f1ecc215f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e4cda7a9986f2d71a6",
+    "url": "/static/css/main.5555ef39.chunk.css"
+  },
+  {
+    "revision": "304214766291ea0cbd14",
+    "url": "/static/js/2.47b5fe32.chunk.js"
+  },
+  {
+    "revision": "15e4cda7a9986f2d71a6",
+    "url": "/static/js/main.f4489d7d.chunk.js"
+  },
+  {
+    "revision": "5688b8f132f44ee061b0",
+    "url": "/static/js/runtime-main.2b49eeea.js"
+  }
+]);
